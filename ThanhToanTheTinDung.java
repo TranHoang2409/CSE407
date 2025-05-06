f01d36012c22284b347412d3da508a7ae66d34b8
tranhoang2409 <bestfriend23102003@gmail.com>
2025-05-06 09:16:23 +0700

    Commit đầu tiên

---
+++ ThanhToanTheTinDung.java
@@ -0,0 +1,9 @@
+package thanhtoan;
+
+public class ThanhToanTheTinDung implements IThanhToan {
+
+    @Override
+    public void ThanhToan(double soTien) {
+        System.out.println("Thanh toán " + soTien + " VNĐ bằng thẻ tín dụng.");
+    }
+}
